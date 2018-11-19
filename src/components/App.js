76ef9d0b06a9181f5c9aae821bddef47ae447d24
Brian Espinosa <b@bje.co>
2018-11-19 15:35:29 -0800

    Remove some basic CRA stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo                 from '../logo.svg';
 import styles               from './App.module.css';
 import data                 from '../resume.json';
 
@@ -8,19 +7,7 @@ class App extends Component {
     return (
       <div className={styles.root}>
         <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h1>{data.user.first_name}</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles.link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{data.user.first_name} {data.user.last_name}</h1>
         </header>
       </div>
     );
